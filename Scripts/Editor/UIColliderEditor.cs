4cdd981d0855aaf531bb53bbcbdf8c1ef9c30aaa
baba_s <shotababa1206@gmail.com>
2019-08-28 18:09:43 +0900

    first commit

---
+++ Scripts/Editor/UIColliderEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace KoganeUnityLib
+{
+	[CanEditMultipleObjects]
+	[CustomEditor( typeof( UICollider ) )]
+	public sealed class UIColliderEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+		}
+	}
+}
---
+++ Scripts/Runtime/UICollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace KoganeUnityLib
+{
+	public sealed class UICollider : Graphic
+	{
+		protected override void Awake()
+		{
+			base.Awake();
+
+			canvasRenderer.cullTransparentMesh = true;
+
+			color = new Color( 1, 1, 1, 0 );
+		}
+	}
+}
